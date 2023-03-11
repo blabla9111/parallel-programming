1e6bf76362fbf408726440a77ce92d68c81f2484
blabla9111 <gindullina_2002@mail.ru>
2023-03-11 14:49:32 +0300

    sort, reduce units. Done a basic functional

--- lab1.c
+++ lab1.c
@@ -8,6 +8,24 @@
 #include <sys/time.h>
 #include <math.h>
 
+
+int find_min_not_zero(int n, double sorted_array[]);
+
+void insertion_sort(int n, double mass[])
+{
+    int newElement, location;
+    for (int i = 1; i < n; i++){
+        newElement = mass[i];
+        location = i - 1;
+        while(location >= 0 && mass[location] > newElement)
+        {
+            mass[location+1] = mass[location];
+            location = location - 1;
+            }
+        mass[location+1] = newElement;
+        }
+}
+
 int main(int args, char* argv[]){
     int i, N, A;
     struct timeval T1, T2;
@@ -49,9 +67,40 @@ int main(int args, char* argv[]){
             M2[j] = M2[j]>=M1[j]? M1[j]:M2[j];
             printf("\nM2[%d] = %f\n",j, M2[j]);
         }
+        printf("\n\nStarted to sort M2 ...\n");
+        insertion_sort(N/2,M2);
+        for (int j = 0; j< N/2;j++){
+            printf("\nM2[%d] = %f\n",j, M2[j]);
+        }
+        double sum_sin_even = 0;
+        int min_not_zero_index = find_min_not_zero(N/2,M2);
+        printf("\nmin_not_zero_index = %d\n", min_not_zero_index);
+        if (min_not_zero_index==-1){
+            printf("\nOnly zeros in array M2\n\n");
+            return -1;
+        }
+        for (int j = 0; j< N/2;j++){
+            double div_to_min = M2[j]/M2[min_not_zero_index];
+            printf("\ndiv_to_min = %f\n",div_to_min);
+            if ((int)floor(div_to_min)%2==0){
+                printf("floor %f = %f",div_to_min,floor(div_to_min));
+                sum_sin_even = sum_sin_even + sin(M2[j]);
+            }
+        }
+        printf("\n\n____________________________\nANSWER sum_sin_even = %f\n_______________________________\n\n", sum_sin_even);
+
     }
     gettimeofday(&T2,NULL);
     delta_ms = 1000 * (T2.tv_sec - T1.tv_sec) + (T2.tv_usec - T1.tv_usec)/1000;
     printf("\nN=%d. Milliseconds passed: %ld\n",N,delta_ms);
     return 0;
+}
+
+int find_min_not_zero(int n, double sorted_array[]) {
+    for (int i = 0; i < n; i++){
+        if (sorted_array[i]!=0){
+            return i;
+        }
+    }
+    return -1;
 }
